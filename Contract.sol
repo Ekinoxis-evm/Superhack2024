b70b74eea99ad74ac9e799bd4f95793e33cf9485
William Martinez <68308482+wmb81321@users.noreply.github.com>
2024-08-11 19:26:58 -0500

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -19,3 +19,24 @@ contract AnimalConservation is ERC721 {
         address[] contributors;    // List of addresses who contributed
         uint256[] contributions;   // Corresponding contribution amounts
     }
+// Mapping to store species information using their IDs
+    mapping(uint256 => Species) public speciesMap;
+
+    // Counter to keep track of the total number of species created
+    uint256 public numberOfSpecies = 0; 
+
+    // Constructor to initialize the ERC721 contract with a name and symbol
+    constructor() ERC721("ConservationNFT", "CNFT") {} 
+
+    // Function to create a new conservation species campaign
+    function createSpecies(
+        string memory _name,
+        string memory _description,
+        uint256 _targetAmount,
+        uint256 _nftPrice,
+        uint256 _maxNFTs,
+        string memory _image
+    ) public {
+        // Ensure there's a limit on NFTs and they have a price
+        require(_maxNFTs > 0, "Maximum NFTs must be greater than 0");
+        require(_nftPrice > 0, "NFT price must be greater than 0");
